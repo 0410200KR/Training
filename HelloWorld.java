06576322a3253d4e594ddc8df55eed413a8c9f37
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-07 16:03:53 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    public static int addition(int a,int b){
+        int result=a+b;
+    }
+    public static void main(String[]args){
+   System.out.println("HELLO WORLD");
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   int b=sc.nextInt();
+   System.out.println(addition(a,b));
+
+   System.err.println('hello kanak');
+
+   System.err.println('hello kanak rajput');
+   
+   int x = 8;
+   System.out.println(x);
+   int y = 8;
+   System.out.println(y);
+   int z = 8;
+   System.out.println(z);
+   int p = 8;
+   System.out.println(p);
+
+
+}
+} 
